c03505f453707494fe34af9878aa09300595be03
HuffDaddy0 <ty.huff24@gmail.com>
2021-05-31 12:04:41 -0400

    database connected correctly

--- db/migrate/20210531145214_create_users.rb
+++ db/migrate/20210531145214_create_users.rb
@@ -3,6 +3,11 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
 
       t.timestamps
+      t.string :email
+      t.string :password_digest
+      t.string :region
+      t.string :position
+      t.string :role
     end
   end
 end
